743444e51139cd2b9119faab1d1fba6cb43d05dd
TANVEERHASSANGETHIB <bscs22144@itu.edu.pk>
2024-10-28 16:30:31 +0500

    Components Impemented

---
+++ Airbnb-Project/src/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from '../Components/Navbar'
+import { Footer } from '../Components/Footer'
+
+export const HomePage = () => 
+    {
+  return (
+    <div>
+        <Navbar/>
+        <Footer/>
+    
+    </div>
+  )
+}
